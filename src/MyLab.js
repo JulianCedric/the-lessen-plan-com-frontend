e4f2b701e0b6ba96229fc441899bbb906b47e04c
JulianCedric <julian.pormentilla@gmail.com>
2021-01-20 05:38:48 -0500

    Updates - MyLab

--- src/MyLab.js
+++ src/MyLab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image, Reveal } from 'semantic-ui-react';
 
 class MyLab extends React.Component {
     state = {  }
@@ -6,6 +7,14 @@ class MyLab extends React.Component {
         return (  
             <div>
                 <p>My Lab</p>
+                <Reveal animated='move'>
+    <Reveal.Content visible>
+      <Image src='https://react.semantic-ui.com/images/wireframe/square-image.png' size='small' />
+    </Reveal.Content>
+    <Reveal.Content hidden>
+      <Image src='https://react.semantic-ui.com/images/avatar/large/chris.jpg' size='small' />
+    </Reveal.Content>
+  </Reveal>
             </div>
         );
     }
--- src/ThinkSpace.js
+++ src/ThinkSpace.js
@@ -4,3 +4,4 @@
 // Next: 
   // Resolve fetch issue ( Login.js - ln.23 - handleSubmitSignUp )
 
+<img src="/images/lessismore.png" alt=""/>
