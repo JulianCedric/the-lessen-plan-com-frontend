260653b925be343ff0b88975b06bd775af23e38e
JulianCedric <julian.pormentilla@gmail.com>
2021-01-25 15:34:24 -0500

    Logo & Title

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className="footer" style={{ color: 'white' }}>
+        <div className="footer" style={{ color: 'grey' }}>
             <div className="footer-copyright text-center py-3" >
                 <h7 className="mediumWhiteText" >meOS Labs © {new Date().toISOString().substring(0, 4)} </h7>
             </div>   
