e0f9283306758b076fbe76678b3087e44534cb77
JulianCedric <julian.pormentilla@gmail.com>
2021-01-24 22:07:32 -0500

    Updates - Navbar Icons

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Icon, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
 
 class Navbar extends React.Component {
     render() { 
