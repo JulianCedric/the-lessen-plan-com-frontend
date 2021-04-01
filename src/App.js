7f198c4fc7e13ed892fc474612bab0e3e2e0eb05
JulianCedric <julian.pormentilla@gmail.com>
2021-04-01 15:01:51 -0400

    UI updates

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ import Home from './Home';
 import Footer from './Footer';
 import SignUp from './SignUp';
 
+// let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
+
+// function largestSubarraySum(array){
+//     let arr = [1, 1, 5, 3, 7, 4, 5, 6, 100, 4]
+//     let sum = arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7] + arr[8] + arr[9] + 23
+//     return sum
+// }
+
+// largestSubarraySum(array)
+// // 16
+
 class App extends React.Component {
   state = {  
     loggedIn: false,
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 class Home extends React.Component {
     state = {  }
     render() { 
         return (  
             <div className='homepage'>
+                <p>+</p>
             </div>
         );
     }
