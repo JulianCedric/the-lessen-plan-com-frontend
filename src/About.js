871ba414e93d1d05903cd67a21cf0df856fd7994
JulianCedric <julian.pormentilla@gmail.com>
2021-04-08 14:08:20 -0400

    Updates to About Page

--- src/About.js
+++ src/About.js
@@ -12,3 +12,13 @@ class About extends React.Component {
 }
  
 export default About;
+
+// 2021.04.08 - THU:
+
+// "The Process:
+
+// 1) Decide what you want to achieve.
+// 2) Try different ways of achieving it until you find one that works for you.
+// 3) Do more of what works. Do less of what doesn’t.
+// 4) Don’t stop doing it until it stops working.
+// 5) Repeat." 
