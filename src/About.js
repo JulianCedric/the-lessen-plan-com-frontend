9d1a5e42a90b719717cbab824281b40dd6e1cb0d
JulianCedric <julian.pormentilla@gmail.com>
2021-05-10 17:17:47 -0400

    Centered contents

--- src/About.js
+++ src/About.js
@@ -33,77 +33,37 @@ class About extends React.Component {
         } else {
             alert("Please enter a valid email address.")
         }
-        
-
-        //     for (let i=0; i<email.length; i++) {
-        //         if ("@" == email[i]) {
-        //             arr.push("Test Pass")
-        //         } else {
-        //             console.log("Test Fail")
-        //         }
-        //     }
-        
-        // if (arr.join('') == "Test Pass") {
-        //     this.setState({renderPass: !this.state.renderPass})
-        // } else {
-        //     console.log("Please enter a valid email address.")
-        // }
     }
-
-    // validator = e => {
-    //     let email = "johndoe@mail.com"
-    //     console.log(email.length)
-    //     let arr = []
-    //     const passOrFail = input => {
-    //         for (let i=0; i<input.length; i++) {
-    //             if ("@" == input[i]) {
-    //                 arr.push("Test Pass")
-    //             } else {
-    //                 console.log("Test Fail")
-    //             }
-    //         }
-    //     }
-    //     console.log(passOrFail(email))
-    //     console.log(arr.join(''))
-    //     if (arr.join('') == "Test Pass") {
-    //         this.setState({pass: !this.state.pass})
-    //     } else {
-    //         console.log("Please enter a valid email address.")
-    //     }
-    // }
-
+        
     render() { 
         return (  
             <div className='about'>
-            //     <Container textAlign='center'>About</Container>
-            <br/>
-            <br/>
-            <p>
-                {!this.state.renderPass
-                    ? "Enter Your Email Address:"
-                    : "Pass"
-                }
-            </p>
-
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Group>
-                    <Form.Input
-                        placeholder='email'
-                        name='email'
-                        value={this.state.email}
-                        onChange={this.handleChange}
-                    />     
-                    <br/>
-                    <Form.Button inverted color='green' content='+' onClick={this.handleClick} />
-                </Form.Group>
-            </Form>
-            
-            
-            <br/>    
-            <br/>    
-            {/* {this.state.pass
-            ? <Button onClick={this.validator} color="green">Test Button</Button> 
-            : <Button onClick={this.validator} secondary>Test Button</Button>} */}
+                <Container textAlign='center'>About</Container>
+                <br/>
+                <br/>
+                <p>
+                    {!this.state.renderPass
+                        ? "Enter Your Email Address:"
+                        : "Pass"
+                    }
+                </p>
+                <div className="email">
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group>
+                            <Form.Input
+                                placeholder='email'
+                                name='email'
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                            />     
+                            <br/>
+                            <br/>
+                            <Form.Button inverted color='green' content='+' onClick={this.handleClick} />
+                        </Form.Group>
+                    </Form>
+                </div>
+                <br/>    
+                <br/>    
             </div>
         );
     }
