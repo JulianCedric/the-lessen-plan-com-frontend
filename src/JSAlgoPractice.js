8fda628e33d4670e5fd039006c7a5dd7c502931f
JulianCedric <julian.pormentilla@gmail.com>
2021-06-22 18:45:32 -0400

    Roman Numeral Converter algo problem-2/26

--- src/JSAlgoPractice.js
+++ src/JSAlgoPractice.js
@@ -31,3 +31,31 @@ function convertToRoman(num) {
   }
   
   console.log("Result:", convertToRoman(2));
+
+  // passing 2/26 tests:
+
+  // I  1
+// V  5
+// X  10
+// L  50
+// C  100
+// D  500
+// M  1000 
+
+function convertToRoman(num) {
+    console.log("num:", num);
+  
+    let romanNumeral = "";
+  
+    while (num > 0) {
+      console.log(num);
+      romanNumeral += "I";
+      num--;
+    } 
+  
+    console.log("romanNumeral:", romanNumeral);
+    return romanNumeral;
+  
+  }
+  
+  console.log("Result:", convertToRoman(3));
