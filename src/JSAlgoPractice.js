413ec126a3296e35d3d21dba1b100f523c85a792
JulianCedric <julian.pormentilla@gmail.com>
2021-06-24 15:25:42 -0400

    Telephone Number Validator algo problem-10/28 passing

--- src/JSAlgoPractice.js
+++ src/JSAlgoPractice.js
@@ -475,3 +475,71 @@ console.log("Result:", telephoneCheck("555-555-5555"));
 // console.log("Result:", telephoneCheck("555 555 5555"));
 // console.log("Result:", telephoneCheck("5555555555"));
 // console.log("Result:", telephoneCheck("1 555 555 5555"));
+
+// 10/28 passing:
+
+telephoneCheck("555-555-5555") should return a boolean.
+
+Passed
+telephoneCheck("1 555-555-5555") should return true.
+
+Passed
+telephoneCheck("1 (555) 555-5555") should return true.
+
+Passed
+telephoneCheck("5555555555") should return true.
+
+Passed
+telephoneCheck("555-555-5555") should return true.
+
+Passed
+telephoneCheck("(555)555-5555") should return true.
+
+Passed
+telephoneCheck("1(555)555-5555") should return true.
+
+telephoneCheck("555-5555") should return false.
+
+telephoneCheck("5555555") should return false.
+
+telephoneCheck("1 555)555-5555") should return false.
+
+Passed
+telephoneCheck("1 555 555 5555") should return true.
+
+Passed
+telephoneCheck("1 456 789 4444") should return true.
+
+Passed
+telephoneCheck("123**&!!asdf#") should return false.
+
+telephoneCheck("55555555") should return false.
+
+telephoneCheck("(6054756961)") should return false.
+
+telephoneCheck("2 (757) 622-7382") should return false.
+
+telephoneCheck("0 (757) 622-7382") should return false.
+
+telephoneCheck("-1 (757) 622-7382") should return false.
+
+telephoneCheck("2 757 622-7382") should return false.
+
+telephoneCheck("10 (757) 622-7382") should return false.
+
+telephoneCheck("27576227382") should return false.
+
+telephoneCheck("(275)76227382") should return false.
+
+telephoneCheck("2(757)6227382") should return false.
+
+telephoneCheck("2(757)622-7382") should return false.
+
+telephoneCheck("555)-555-5555") should return false.
+
+telephoneCheck("(555-555-5555") should return false.
+
+Passed
+telephoneCheck("(555)5(55?)-5555") should return false.
+
+telephoneCheck("55 55-55-555-5") should return false.
