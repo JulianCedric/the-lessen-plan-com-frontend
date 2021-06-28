5a21b2e40354efda1108e0a9823884ca6077d7be
JulianCedric <julian.pormentilla@gmail.com>
2021-06-28 10:38:45 -0400

    JSAlgoPractice2 - Regex

--- src/JSAlgoPractice2.js
+++ src/JSAlgoPractice2.js
@@ -7,3 +7,19 @@ let userCheck = /^[a-z]/; // Change this line
 let result = userCheck.test(username);
 
 console.log("result:", result);
+
+// 6/13 passing:
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]/; // Change this line
+let result = userCheck.test(username);
+
+console.log("result:", result);
+
+// 8/13:
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z]/i; // Change this line
+let result = userCheck.test(username);
+
+console.log("result:", result);
