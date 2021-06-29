297cbe87f12173f9c4262beab5885c6afcd4af26
JulianCedric <julian.pormentilla@gmail.com>
2021-06-29 13:29:56 -0400

    JSAlgoPractice3

--- src/JSAlgoPractice3.js
+++ src/JSAlgoPractice3.js
@@ -1,2 +1,26 @@
 // 2021.06.29 - TUE:
 
+// drafts for Steamroller algo problem:
+
+function steamrollArray(arr) {
+
+    var newArr = [];
+  
+    while (arrInDS(arr)) {
+      newArr.push(arr);
+      // arr = arr.flat();
+    }
+  
+    console.log(arr)
+    return arr;
+  
+  
+  }
+  
+  function arrInDS(ds) {
+    return ds.some(dPoint => {
+      return Array.isArray(dPoint);  
+    })
+  }
+  
+  steamrollArray([1, [2], [3, [[4]]]]);
