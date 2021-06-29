e66adc744beb3a38c16f8d2a5e6526e14b1e9514
JulianCedric <julian.pormentilla@gmail.com>
2021-06-29 13:34:57 -0400

    JSAlgoPractice3

--- src/JSAlgoPractice3.js
+++ src/JSAlgoPractice3.js
@@ -24,3 +24,15 @@ function steamrollArray(arr) {
   }
   
   steamrollArray([1, [2], [3, [[4]]]]);
+
+  // Binary Agents (from past progress):
+
+  function binaryAgent(str) {
+    console.log("str:", str);
+  
+  
+  
+  
+  }
+  
+  console.log("Result:", binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
