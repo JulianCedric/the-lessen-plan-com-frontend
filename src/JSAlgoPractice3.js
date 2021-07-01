0803b68a7d9bade4d519134e28994d0818f143c5
JulianCedric <julian.pormentilla@gmail.com>
2021-07-01 12:51:32 -0400

    JSAlgoPractice4.js

--- src/JSAlgoPractice3.js
+++ src/JSAlgoPractice3.js
@@ -1,3 +1,34 @@
+// 2021.07.01 - THU:
+
+function smallestCommons(arr) {
+
+    let sortedArr = arr.sort((a,b) => a-b);
+    console.log("sortedArr:", sortedArr);
+  
+    let lowerNum = sortedArr[0];
+    let higherNum = sortedArr[1];
+    // console.log(lowerNum, higherNum);
+  
+    let range = rangeMaker(lowerNum, higherNum);
+  
+  
+  }
+  
+  function rangeMaker(lowerNumber, higherNumber) {
+    let arr = [];
+    let counter = lowerNumber;
+  
+    while (counter <= higherNumber) {
+      arr.push(counter);
+      counter += 1;
+    }
+    console.log("arr:", arr);
+    return arr;
+  }
+  
+  console.log("Result:", smallestCommons([1,5]));
+
+
 // 2021.06.30 - WED: 
 
 // Smallest Common Multiple: 
