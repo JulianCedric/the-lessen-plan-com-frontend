26b5a18eefdf9033db7a5a41e0b44efb18f4f036
JulianCedric <julian.pormentilla@gmail.com>
2021-07-06 05:46:36 -0400

    JSAlgoPractice5

--- src/JSAlgoPractice5.js
+++ src/JSAlgoPractice5.js
@@ -17,3 +17,39 @@ function convertToRoman(num) {
 console.log("Result:", convertToRoman(9)); 
 
 // 
+// I  1
+// V  5
+// X  10
+// L  50
+// C  100
+// D  500
+// M  1000 
+
+function convertToRoman(num) {
+    const map = {
+      M:  1000,
+      CM: 900,
+      D:  500,
+      CD: 400,
+      C:  100,
+      XC: 90,
+      L:  50,
+      XL: 40,
+      X:  10,
+      IX: 9,
+      V:  5,
+      IV: 4,
+      I:  1,
+    };
+    let result = '';
+    
+    // main algorithm
+    
+    console.log("result:", result);
+    return result;
+  };
+  
+console.log("Result:", convertToRoman(9)); 
+
+//
+
