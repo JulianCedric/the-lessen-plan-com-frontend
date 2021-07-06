4b8b597fb3c4aef8b2ba731da9464522b9beb354
JulianCedric <julian.pormentilla@gmail.com>
2021-07-06 05:44:56 -0400

    JSAlgoPractice5

--- src/JSAlgoPractice5.js
+++ src/JSAlgoPractice5.js
@@ -1,2 +1,19 @@
 // 2021.07.06 - TUE:
 
+// Roman Numeral Converter
+
+// I  1
+// V  5
+// X  10
+// L  50
+// C  100
+// D  500
+// M  1000 
+
+function convertToRoman(num) {
+  
+}
+  
+console.log("Result:", convertToRoman(9)); 
+
+// 
