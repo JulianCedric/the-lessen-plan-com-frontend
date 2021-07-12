71d779ff647280d9eab836fff35d90cbfbb8f1ff
JulianCedric <julian.pormentilla@gmail.com>
2021-07-11 23:19:01 -0400

    LeetCode Practice

--- src/LeetCodePractice1.js
+++ src/LeetCodePractice1.js
@@ -53,3 +53,90 @@ console.log("Result:", maximumWealth(accts));
     // Create array to collect respective sums of each customer's accounts
     // Create hash whose keys represent the ith customer/s, and values initialized to 0
     // Return highest sum
+
+//
+
+var accts = [[1,2,3],[3,2,1]];
+
+function maximumWealth(accounts) {
+    
+    var hash = {};
+    
+    for (let i=0; i<accounts.length; i++) {
+      var total = 0;
+        for (let j=0; j<accounts[i].length; j++) {
+          total += accounts[i][j];
+        }
+        hash[i] = total;
+    }
+    console.log("hash:", hash);
+    
+    var arr = Object.values(hash);
+    console.log("arr:", arr);
+
+    var maxVal = arr[0];
+
+    for (let i=1; i<arr.length; i++) {
+      if (arr[i] > maxVal) {
+        maxVal = arr[i];
+      }
+      console.log("maxVal:", maxVal);
+    }
+
+    return maxVal;
+    
+};
+
+console.log("Result:", maximumWealth(accts));
+
+// Pseudocode: 
+
+    // For loop through each customer's accounts
+    // For loop through each amount in each customer's accounts
+    // Create array to collect respective sums of each customer's accounts
+    // Create hash whose keys represent the ith customer/s, and values initialized to 0
+    // Return highest sum
+    // ... 
+
+//
+
+var accts = [[1,2,3],[3,2,1]];
+
+function maximumWealth(accounts) {
+    
+    var hash = {};
+    
+    for (let i=0; i<accounts.length; i++) {
+      var total = 0;
+        for (let j=0; j<accounts[i].length; j++) {
+          total += accounts[i][j];
+        }
+        hash[i] = total;
+    }
+    console.log("hash:", hash);
+    
+    var arr = Object.values(hash);
+    console.log("arr:", arr);
+
+    var maxVal = arr[0];
+
+    for (let i=1; i<arr.length; i++) {
+      if (arr[i] > maxVal) {
+        maxVal = arr[i];
+      }
+      console.log("maxVal:", maxVal);
+    }
+
+    return maxVal;
+    
+};
+
+console.log("Result:", maximumWealth(accts));
+
+// Pseudocode: 
+
+    // For loop through each customer's accounts
+    // For loop through each amount in each customer's accounts
+    // Create hash whose keys represent the ith customer/s, and values are totals of each account
+    // Determine highest sum, assigned to maxVal
+    // Return maxVal
