602a3f481ee21b01a0b96d990238709c70292b46
JulianCedric <julian.pormentilla@gmail.com>
2021-07-11 23:05:16 -0400

    LeetCode Practice

--- src/LeetCodePractice1.js
+++ src/LeetCodePractice1.js
@@ -22,3 +22,34 @@ var maximumWealth = function(accounts) {
     // Create array to collect respective sums of each customer's accounts
     // Create hash whose keys represent the ith customer/s, and values initialized to 0
     // Return highest sum
+
+// 
+
+var accts = [[1,2,3],[3,2,1]];
+
+function maximumWealth(accounts) {
+    
+    var hash = {};
+    
+    for (let i=0; i<accounts.length; i++) {
+      var arr = [];
+        for (let j=0; j<accounts[i].length; j++) {
+          arr.push(accounts[i][j]);
+        }
+        hash[i] = arr;
+    }
+    console.log("hash:", hash);
+
+    
+    
+};
+
+console.log("Result:", maximumWealth(accts));
+
+// Pseudocode: 
+
+    // For loop through each customer's accounts
+    // For loop through each amount in each customer's accounts
+    // Create array to collect respective sums of each customer's accounts
+    // Create hash whose keys represent the ith customer/s, and values initialized to 0
+    // Return highest sum
