eb2289ccb8f04191cc2fdeab47a4b0e99b622da1
JulianCedric <julian.pormentilla@gmail.com>
2021-07-16 09:57:11 -0400

    LeetCode Practice

---
+++ src/LeetCodePractice2.js
@@ -0,0 +1,18 @@
+//2021.07.16 - FRI:
+
+var array = [1,2,3,4];
+
+function runningSum(nums) {
+
+  let sum = 0;
+  let arr = [];
+
+  for (let i=0; i<nums.length; i++) {
+    sum += nums[i];
+    arr.push(sum);
+  }
+  console.log(arr);
+
+}
+
+console.log("\n Result:", runningSum(array));
