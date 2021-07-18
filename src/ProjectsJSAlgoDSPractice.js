323376e69648e64ed242deca40cbab007ed15910
JulianCedric <julian.pormentilla@gmail.com>
2021-07-18 18:03:50 -0400

    Final Free Code Camp 5 Projects

--- src/ProjectsJSAlgoDSPractice.js
+++ src/ProjectsJSAlgoDSPractice.js
@@ -1,6 +1,60 @@
 // 2021.07.18 - SUN:
 
-// Roman Numeral Converter 
+// Palindrome Checker (Project: 1 of 5)
+
+function palindrome(str) {
+    let newStr = str.toLowerCase().replace(/[^a-z\d]/g, '');
+    return newStr.split('').reverse().join('') === newStr;
+  }
+  
+  console.log(palindrome("_eye"));
+  
+  // Code below this line is from a previous attempt at solving Palidrome Checker w/o Regex:
+  
+  // function palindrome(str) {
+  
+  //   let str = str.split('');
+  //   // console.log("str:", str);
+  
+  //   str = str.map(elem => {
+  //     return elem.toLowerCase();
+  //   });
+  //   // console.log("str:", str);
+  
+  //   for (let i=0; i<str.length; i++) {
+  //     if (str[i] === " " || str[i] === "_" || str[i] === "," || str[i] === "." || str[i] === "-" || str[i] === ":") {
+  //       str.splice(i, 1);
+  //       str.length - 1;
+  //     }
+  //   }
+  
+  //   let newStr = str.join('');
+  //   let reversedStrArr = str.reverse();
+  //   let reversedStr = reversedStrArr.join('');
+  
+  //   let newStrArr = newStr.split(' ');
+  //   newStr = newStrArr.join('');
+  
+  //   let newReversedStrArr = reversedStr.split(' ');
+  //   reversedStr = newReversedStrArr.join('');
+    
+  //   if (newStr == reversedStr) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  
+  console.log("Result:", palindrome("0_0 (: /-\ :) 0-0"));
+  // console.log("Result:", palindrome("A man, a plan, a canal. Panama")); 
+  // console.log("Result:", palindrome("My age is 0, 0 si ega ym."));
+  // console.log("Result:", palindrome("_eye"));
+  // console.log("Result:", palindrome("race car"));
+  // console.log("Result:", palindrome("never odd or even"));
+
+
+
+// Roman Numeral Converter (Project: 2 of 5)
 
 // I  1
 // V  5
