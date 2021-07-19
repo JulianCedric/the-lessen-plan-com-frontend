8c07466afb913b97e65ebb94742b67765212ab28
JulianCedric <julian.pormentilla@gmail.com>
2021-07-19 18:57:44 -0400

    LeetCode Practice

--- src/LeetCodePractice2.js
+++ src/LeetCodePractice2.js
@@ -1,3 +1,41 @@
+// 2021.07.19 - MON:
+
+// Shortest Distance to a Character:
+
+var str = "loveleetcode";
+var char = "e";
+
+function shortestToChar(s, c) {
+    let output = [];
+    
+    s = s.split("");
+
+    let curE = s.indexOf(c);
+    console.log("curE:", curE);
+
+
+    for (let i=0; i<s.length; i++) {
+        if (s[i] === c) {
+            curE = i
+        }
+
+        let left = Math.abs(i - curE);
+        let right = Math.abs(i - s.indexOf(c,i));
+        
+        // console.log("Left=" + left)
+        // console.log("Right=" + right)
+
+        let distance = Math.min(left, right)
+        
+        output.push(distance)
+    }
+    
+  return output
+};
+
+console.log("Result:", shortestToChar(str, char));
+
+
 //2021.07.16 - FRI:
 
 var array = [1,2,3,4];
