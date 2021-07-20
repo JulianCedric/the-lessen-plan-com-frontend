e5edd032d2abae557faddd4fca3983c8e5bdf672
JulianCedric <julian.pormentilla@gmail.com>
2021-07-20 14:02:09 -0400

    Updates to App.js

--- src/App.js
+++ src/App.js
@@ -11,17 +11,6 @@ import Footer from './Footer';
 import SignUp from './SignUp';
 import About from './About';
 
-// let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4]
-
-// function largestSubarraySum(array){
-//     let arr = [1, 1, 5, 3, 7, 4, 5, 6, 100, 4]
-//     let sum = arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7] + arr[8] + arr[9] + 23
-//     return sum
-// }
-
-// largestSubarraySum(array)
-// // 16
-
 class App extends React.Component {
   state = {  
     loggedIn: false,
