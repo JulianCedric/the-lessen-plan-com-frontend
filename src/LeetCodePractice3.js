e9d3a2b788bad8f35b3ddbb8f395e93d50758085
JulianCedric <julian.pormentilla@gmail.com>
2021-07-21 12:35:09 -0400

    LeetCode Practice

--- src/LeetCodePractice3.js
+++ src/LeetCodePractice3.js
@@ -81,4 +81,99 @@ function longestCommonPrefix(strs) {
 
 };
 
+console.log("Result:", longestCommonPrefix(strings));  
+
+// 
+
+// var strings = ["flower","flow","flight"];
+var strings = ["cir","car"];
+
+function longestCommonPrefix(strs) {
+
+  let comparisonWord = strs[0];
+
+  let hash = {};
+
+  let arr = [];
+
+  for (let comparisonLetter of comparisonWord) {
+    console.log("ITERATION:", "comparisonLetter:", comparisonLetter);
+    hash[comparisonLetter] = 1;
+  
+    for (let i=1; i<strs.length; i++) {
+      if (strs[i].indexOf(comparisonLetter) !== -1) {
+        hash[comparisonLetter] += 1;
+      }
+    }
+
+    if (hash[comparisonLetter] === strs.length) {
+      arr.push(comparisonLetter);      
+    }
+
+    if (arr.length > 1) {
+      for (let j=0; j<arr.length; j++) {
+        if (Math.abs(comparisonWord.indexOf(arr[j]) - comparisonWord.indexOf(arr[j+1]) > 1)) {
+          arr.pop();
+        }
+      }
+    }
+
+  }
+  console.log("arr:", arr);
+
+  let longest = arr.join("");
+  console.log("longest:", longest);
+
+  return longest;
+
+};
+
+console.log("Result:", longestCommonPrefix(strings)); 
+
+//
+
+var strings = ["flower","flow","flight"];
+// var strings = ["cir","car"];
+
+function longestCommonPrefix(strs) {
+
+  let comparisonWord = strs[0];
+
+  let hash = {};
+
+  let arr = [];
+
+  for (let comparisonLetter of comparisonWord) {
+    console.log("ITERATION:", "comparisonLetter:", comparisonLetter);
+    hash[comparisonLetter] = 1;
+  
+    for (let i=1; i<strs.length; i++) {
+      if (strs[i].indexOf(comparisonLetter) !== -1) {
+        hash[comparisonLetter] += 1;
+      }
+    }
+
+    if (hash[comparisonLetter] === strs.length) {
+      arr.push(comparisonLetter);      
+    }
+
+  }
+
+  if (arr.length > 1) {
+    for (let j=0; j<arr.length-1; j++) {
+      console.log(Math.abs(strs[0].indexOf(arr[j]) - strs[0].indexOf(arr[j+1])));
+      if (Math.abs(strs[0].indexOf(arr[j]) - strs[0].indexOf(arr[j+1])) > 1) {
+        arr.pop();
+      }
+    }
+  }
+  console.log("arr:", arr);
+
+  let longest = arr.join("");
+  console.log("longest:", longest);
+
+  return longest;
+
+};
+
 console.log("Result:", longestCommonPrefix(strings)); 
