8de0c7d5357abf0a5bb2bcb8e001dd91208d4fa8
JulianCedric <julian.pormentilla@gmail.com>
2021-07-30 12:13:12 -0400

    React Practice

--- src/ReactPractice.js
+++ src/ReactPractice.js
@@ -1,5 +1,45 @@
 // 2021.07.30 - FRI:
 
+// Add Event Listeners:
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        message: ''
+      };
+      this.handleEnter = this.handleEnter.bind(this);
+      this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+    // Change code below this line
+    componentDidMount() {
+      document.addEventListener("keydown", this.handleKeyPress);
+    }
+    componentWillUnmount() {
+      document.removeEventListener("keydown", this.handleKeyPress);
+    }
+    // Change code above this line
+    handleEnter() {
+      this.setState((state) => ({
+        message: state.message + 'You pressed the enter key! '
+      }));
+    }
+    handleKeyPress(event) {
+      if (event.keyCode === 13) {
+        this.handleEnter();
+      }
+    }
+    render() {
+      return (
+        <div>
+          <h1>{this.state.message}</h1>
+        </div>
+      );
+    }
+  };
+
+//
+
 // Use the LifCycle Method componentDidMount:
 
 class MyComponent extends React.Component {
