695a7bfad976a207f7424e5f5f6b774ca3170686
JulianCedric <julian.pormentilla@gmail.com>
2021-07-30 13:09:55 -0400

    React Practice - Use && for a More Concise Conditional

--- src/ReactPractice.js
+++ src/ReactPractice.js
@@ -1,5 +1,34 @@
 // 2021.07.30 - FRI:
 
+// Use && for a More Concise Conditional:
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        display: true
+      }
+      this.toggleDisplay = this.toggleDisplay.bind(this);
+    }
+    toggleDisplay() {
+      this.setState(state => ({
+        display: !state.display
+      }));
+    }
+    render() {
+      // Change code below this line
+  
+      const display = this.state.display && <h1>Displayed!</h1>;
+  
+      return (
+         <div>
+           <button onClick={this.toggleDisplay}>Toggle Display</button>
+          {display}
+         </div>
+      );
+    }
+  };
+
 // Render with an If-Else Condition:
 
 class MyComponent extends React.Component {
