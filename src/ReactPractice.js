855f16e730304a1ea258e57fc9af018cae92a385
JulianCedric <julian.pormentilla@gmail.com>
2021-07-30 12:03:10 -0400

    React Practice

--- src/ReactPractice.js
+++ src/ReactPractice.js
@@ -1,6 +1,35 @@
 // 2021.07.30 - FRI:
 
-// Use the LifeCycle Method componentWillMount
+// Use the LifCycle Method componentDidMount:
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        activeUsers: null
+      };
+    }
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({
+          activeUsers: 1273
+        });
+      }, 2500);
+    }
+    render() {
+      return (
+        <div>
+          {/* Change code below this line */}
+          <h1>Active Users: {this.state.activeUsers} </h1>
+          {/* Change code above this line */}
+        </div>
+      );
+    }
+  }
+
+//
+
+// Use the LifeCycle Method componentWillMount:
 
 class MyComponent extends React.Component {
     constructor(props) {
