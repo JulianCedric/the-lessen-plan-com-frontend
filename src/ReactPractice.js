498f51980b33d2c2649636c5bcd5091538745ee3
JulianCedric <julian.pormentilla@gmail.com>
2021-07-30 14:05:52 -0400

    React Practice - Change Inline CSS Conditionally Based on Component State

--- src/ReactPractice.js
+++ src/ReactPractice.js
@@ -1,5 +1,46 @@
 // 2021.07.30 - FRI:
 
+// Change Inline CSS Conditionally Based on Component State:
+
+class GateKeeper extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: ''
+      };
+      this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(event) {
+      this.setState({ input: event.target.value })
+    }
+    render() {
+      let inputStyle = {
+        border: '1px solid black'
+      };
+      // Change code below this line
+  
+      console.log("this.state.input.length:", this.state.input.length);
+  
+      if (this.state.input.length > 15) {
+        inputStyle.border = '3px solid red'
+      }
+  
+      console.log("inputStyle.border:", inputStyle.border);
+  
+      // Change code above this line
+      return (
+        <div>
+          <h3>Don't Type Too Much:</h3>
+          <input
+            type="text"
+            style={inputStyle}
+            value={this.state.input}
+            onChange={this.handleChange} />
+        </div>
+      );
+    }
+  };
+
 // Render Conditionally from Props:
 
 class Results extends React.Component {
