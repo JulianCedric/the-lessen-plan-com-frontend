ee3db19e60c07bb8955895412aae8b2d093ca578
JulianCedric <julian.pormentilla@gmail.com>
2021-07-30 14:52:12 -0400

    Redux Practice - Create a Redux Store

--- src/ReduxPractice.js
+++ src/ReduxPractice.js
@@ -1,2 +1,13 @@
 // 2021.07.30 - FRI: 
 
+// Create a Redux Store:
+
+const reducer = (state = 5) => {
+    return state;
+  }
+  
+  // Redux methods are available from a Redux object
+  // For example: Redux.createStore()
+  // Define the store here:
+  
+  const store = Redux.createStore(reducer)
