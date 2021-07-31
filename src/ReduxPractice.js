67dc424aef09345e7590f3dbf3d37ac363bbdcf7
JulianCedric <julian.pormentilla@gmail.com>
2021-07-31 15:02:39 -0400

    Redux Practice - Dispatch an Action Event

--- src/ReduxPractice.js
+++ src/ReduxPractice.js
@@ -1,5 +1,21 @@
 // 2021.07.31 - SAT: 
 
+// Dispatch an Action Event:
+
+const store = Redux.createStore(
+    (state = {login: false}) => state
+  );
+  
+  const loginAction = () => {
+    return {
+      type: 'LOGIN'
+    }
+  };
+  
+  // Dispatch the action here:
+  
+  store.dispatch(loginAction())
+
 // Define an Action Creator:
 
 const action = {
