344597749f96f76aef9ca31c0ff9ea4973be6e41
JulianCedric <julian.pormentilla@gmail.com>
2021-07-31 15:18:47 -0400

    Redux Practice - Handle an Action in the Store

--- src/ReduxPractice.js
+++ src/ReduxPractice.js
@@ -1,5 +1,31 @@
 // 2021.07.31 - SAT: 
 
+// Handle an Action in the Store:
+
+const defaultState = {
+    login: false
+  };
+  
+  const reducer = (state = defaultState, action) => {
+    // Change code below this line
+  
+    if (action.type === 'LOGIN') {
+      return {login: true};
+    } else {
+      return state;
+    }
+  
+    // Change code above this line
+  };
+  
+  const store = Redux.createStore(reducer);
+  
+  const loginAction = () => {
+    return {
+      type: 'LOGIN'
+    }
+  };
+
 // Dispatch an Action Event:
 
 const store = Redux.createStore(
