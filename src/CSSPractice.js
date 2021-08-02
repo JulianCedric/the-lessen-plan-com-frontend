7394a93552caa89457be9f77c8b337a9670d8e04
JulianCedric <julian.pormentilla@gmail.com>
2021-08-02 19:51:33 -0400

    CSS Practice

--- src/CSSPractice.js
+++ src/CSSPractice.js
@@ -137,3 +137,26 @@
     <button type="submit">Submit</button>
   </form>
 </main>
+
+<style>
+  .red-text {
+    color: #FF0000;
+  }
+  .green-text {
+    color: #00FF00;
+  }
+  .dodger-blue-text {
+    color: #1E90FF;
+  }
+  .orange-text {
+    color: #FFA500;
+  }
+</style>
+
+<h1 class="red-text">I am red!</h1>
+
+<h1 class="green-text">I am green!</h1>
+
+<h1 class="dodger-blue-text">I am dodger blue!</h1>
+
+<h1 class="orange-text">I am orange!</h1>
