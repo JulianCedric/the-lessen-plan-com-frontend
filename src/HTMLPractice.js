7529d4f5b77159a79cf618d592b0640d584efafd
JulianCedric <julian.pormentilla@gmail.com>
2021-08-02 14:44:20 -0400

    HTML Practice

--- src/HTMLPractice.js
+++ src/HTMLPractice.js
@@ -9,3 +9,11 @@
 <h1>Hello World</h1>
 <h2>CatPhotoApp</h2>
 <p>Hello Paragraph</p>
+<p>kitty ipsum dolor sit amet</p>
+
+<h2>CatPhotoApp</h2>
+
+<main>
+    <p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>
+    <p>Purr jump eat the grass rip the couch scratched sunbathe, shed everywhere rip the couch sleep in the sink fluffy fur catnip scratched.</p>
+</main>
