b4dcc586c4439407947e743565d4a8531a778585
JulianCedric <julian.pormentilla@gmail.com>
2021-08-03 18:57:10 -0400

    Redux Practice

--- src/ReduxPractice.js
+++ src/ReduxPractice.js
@@ -1,3 +1,45 @@
+// 2021.08.03 - TUE:
+
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+
+  switch (action.type) {
+    case LOGIN: 
+      return {
+        authenticated: true
+      }
+    case LOGOUT: 
+      return {
+        authenticated: false
+      }
+
+    default:
+      return state;
+
+  }
+
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: LOGIN
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: LOGOUT
+  }
+};
+
 // 2021.07.31 - SAT: 
 
 // Use a Switch Statement to Handle Multiple Actions: 
