16dae47d9e2b8749064c874278feece4713b9e28
JulianCedric <julian.pormentilla@gmail.com>
2021-08-17 19:29:38 -0400

    General Practice

--- src/GeneralPractice.js
+++ src/GeneralPractice.js
@@ -2,8 +2,16 @@
 
 const fahrenheit = [23,140,212,41];
 
-function getCelsius() {
+function getCelsius(arr) {
+    const newArr = [];
     
+    arr.map(num => {
+        newArr.push((num - 32) * (5/9));
+    })
+
+    return newArr;
 }
+
+console.log("getCelsius(fahrenheit):", getCelsius(fahrenheit));
 
 // (40°F − 32) × 5/9 = 4.444°C
