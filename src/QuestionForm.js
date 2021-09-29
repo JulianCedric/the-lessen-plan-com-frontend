00bb7600c8790517f8ee3ff65c471b6f46b1e1c9
JulianCedric <julian.pormentilla@gmail.com>
2021-09-28 21:43:04 -0400

    Question From updates

--- src/QuestionForm.js
+++ src/QuestionForm.js
@@ -1,41 +1,36 @@
 import React from 'react';
-import './App.css';
-import { Form } from 'semantic-ui-react';
+import { Grid, Header, Segment } from 'semantic-ui-react';
 
 class QuestionForm extends React.Component {
-    state = {  
-        question: ""
-    }
-
-    handleChange = e => {
-        console.log("User is typing.. ")
-        this.setState({[e.target.name]: e.target.value})
-    }
-
-    handleSubmit = e => {
-        console.log("User submitted a question.")
-        console.log("Current state of question:", this.state.question);
-    
-        const newQuestion = this.state.question;
-        console.log(newQuestion);
-        this.props.handleUserSubmit(newQuestion);
-        this.setState({ question: "" });
-    }
-
     render() { 
-        return (  
-            <div className="question-form">
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group>
-                        <Form.Input 
-                            placeholder='Your Question.. '
-                            name='question'
-                            value={this.state.question}
-                            onChange={this.handleChange}
-                        />
-                        <Form.Button content="Submit"/>
-                    </Form.Group>
-                </Form>
+        return (
+        
+            <div>
+                {/* <Header as='h3' content='Doubling Grid Container' style={style.h3} textAlign='center' /> */}
+
+                <Header
+                as='h3'
+                content='Routines'
+                // style={style.h3}
+                textAlign='center'
+                />
+                <Grid container columns={3} doubling stackable>
+                <Grid.Column>
+                    <Segment>Time-Blocked DWS</Segment>
+                </Grid.Column>
+                <Grid.Column>
+                    <Segment>Routine 2</Segment>
+                </Grid.Column>
+                <Grid.Column>
+                    <Segment>Routine 3</Segment>
+                </Grid.Column>
+                <Grid.Column>
+                    <Segment>Routine 4</Segment>
+                </Grid.Column>
+                <Grid.Column>
+                    <Segment>Routine 5</Segment>
+                </Grid.Column>
+                </Grid>
             </div>
         );
     }
@@ -43,4 +38,51 @@ class QuestionForm extends React.Component {
  
 export default QuestionForm;
 
-// test
+//
+
+// import React from 'react';
+// import './App.css';
+// import { Form } from 'semantic-ui-react';
+
+// class QuestionForm extends React.Component {
+//     state = {  
+//         question: ""
+//     }
+
+//     handleChange = e => {
+//         console.log("User is typing.. ")
+//         this.setState({[e.target.name]: e.target.value})
+//     }
+
+//     handleSubmit = e => {
+//         console.log("User submitted a question.")
+//         console.log("Current state of question:", this.state.question);
+    
+//         const newQuestion = this.state.question;
+//         console.log(newQuestion);
+//         this.props.handleUserSubmit(newQuestion);
+//         this.setState({ question: "" });
+//     }
+
+//     render() { 
+//         return (  
+//             <div className="question-form">
+//                 <Form onSubmit={this.handleSubmit}>
+//                     <Form.Group>
+//                         <Form.Input 
+//                             placeholder='Your 1Question.. '
+//                             name='question'
+//                             value={this.state.question}
+//                             onChange={this.handleChange}
+//                         />
+//                         <Form.Button content="Submit"/>
+//                     </Form.Group>
+//                 </Form>
+//             </div>
+//         );
+//     }
+// }
+ 
+// export default QuestionForm;
+
+// // test
