1dda991f85d4d45619e045c34697dc66bce12b2f
JulianCedric <julian.pormentilla@gmail.com>
2021-10-03 11:28:11 -0400

    Progressbar - Updates

--- src/Timer.js
+++ src/Timer.js
@@ -2,12 +2,25 @@ import React from 'react';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
+const red = '#FF4500';
+const blue = '#6495ED';
 
 function Timer() {
     return (
         <div>
             <h1>Timer</h1>
-            <CircularProgressbar value={77} text={`${77}%`} />;
+
+            <CircularProgressbar
+                value={77}
+                // text={minutes + ':' + seconds}
+                text={'77%'}
+                styles={buildStyles({
+                textColor:'#fff',
+                // pathColor:mode === 'work' ? red : green,
+                tailColor:'rgba(255,255,255,.2)',
+                })} 
+            />
+
         </div>
     )
 }
